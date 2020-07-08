3ea12221dfa21fc36495416b1f26c74dac2be8e1
steams <radclifferobs@gmail.com>
2020-07-08 16:53:26 -0500

    Use consul for configs

--- config/config.go
+++ config/config.go
@@ -1,31 +1,79 @@
 package config
 
 import (
+	"fmt"
 	"os"
+
+	"github.com/spf13/viper"
+	_ "github.com/spf13/viper/remote"
 )
 
 var Conf *Config
 
 func InitConf() {
 	Conf = new(Config)
-	Conf.SetEnv()
+	Conf.setEnv()
+	if err := Conf.setupViper(); err != nil {
+		panic(err)
+	}
+	Conf.setupConfig()
 }
 
 type Config struct {
-	Env string
+	Env        string
+	ConsulHost string
+	ConsulPort string
+	MongoRC    []string
 }
 
-func (c *Config) SetEnv() {
+func (c *Config) setEnv() {
 	env := os.Getenv("ENV")
 	if env == DEV {
 		c.Env = DEV
+		c.ConsulHost = CONSUL_HOST_CLUSTER
+		c.ConsulPort = CONSUL_PORT_CLUSTER
 	} else if env == STAGE {
 		c.Env = STAGE
+		c.ConsulHost = CONSUL_HOST_CLUSTER
+		c.ConsulPort = CONSUL_PORT_CLUSTER
 	} else if env == PROD {
 		c.Env = PROD
+		c.ConsulHost = CONSUL_HOST_CLUSTER
+		c.ConsulPort = CONSUL_PORT_CLUSTER
 	} else {
 		c.Env = WORKSTATION
+		c.ConsulHost = CONSUL_HOST_DEV
+		c.ConsulPort = CONSUL_PORT_DEV
 	}
+}
+
+func (c *Config) setupViper() error {
+	consulUrl := fmt.Sprintf("%s:%s", c.ConsulHost, c.ConsulPort)
+	if err := viper.AddRemoteProvider("consul", consulUrl, CONSUL_KV); err != nil {
+		return err
+	}
+	viper.SetConfigType("json")
+	if err := viper.ReadRemoteConfig(); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (c *Config) setupConfig() {
+	if c.GetEnv() == WORKSTATION {
+		c.MongoRC = MONGOHOSTS_WORKSTATION
+
+	} else {
+		mongoRCI := viper.Get("mongo_rc").([]interface{})
+		var mongoRC []string
+		for _, mongoHost := range mongoRCI {
+			mongoRC = append(mongoRC, mongoHost.(string))
+		}
+
+		c.MongoRC = mongoRC
+	}
+
 }
 
 func (c *Config) GetEnv() string {
--- stores/const.go
+++ stores/const.go
@@ -3,12 +3,3 @@ package stores
 const (
 	DB_NAME = "{{<db_name>}}"
 )
-
-var (
-	MONGOHOSTS_WORKSTATION = []string{"localhost:27017"}
-	MONGOHOSTS_CLUSTER     = []string{
-		"{{<mongo_rs>}}-mongodb-replicaset-0",
-		"{{<mongo_rs>}}-mongodb-replicaset-1",
-		"{{<mongo_rs>}}-mongodb-replicaset-2",
-	}
-)
